65ccb3131887b420df971f2d6b28170ffd5f9548
ginabak <gina@ginabak.com>
2020-03-27 12:18:34 -0400

    Matt, Gina, Devan pairing

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PokemonType } from './PokemonType';
 
 export function Pokedex() {
+  const [selectedPokemon, setSelectedPokemon] = useState(undefined);
+
+  async function getPokemonInfo(name) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`;
+    // Start fetching data from PokeAPI
+    // let data = await fetch(url).then(function(response) {
+    //   return response.json();
+    // });
+    // console.log(data);
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // console.log(data);
+    // Update the React state with our Pokemon data
+    setSelectedPokemon(data);
+  }
+
   return (
     <div>
-      <h4>
-        Edit <code>src/Pokedex.js</code> and save to reload.
-      </h4>
+      {selectedPokemon && (
+        <div>
+          {selectedPokemon.species.name}
+          {selectedPokemon.types.map((type) => {
+            console.log(type);
+            return <PokemonType type={type.type.name} />
+          })}
+        </div>
+      )}
+      <button onClick={() => getPokemonInfo("squirtle")}>Squirtle</button>
+      <button onClick={() => getPokemonInfo("charmander")}>Charmander</button>
+      <button onClick={() => getPokemonInfo("bulbasaur")}>Bulbasaur</button>
+      <button onClick={() => getPokemonInfo("pidgeot")}>Pidgeot</button>
+
     </div>
   );
 }
